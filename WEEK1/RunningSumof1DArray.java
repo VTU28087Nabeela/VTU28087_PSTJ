f21510394a570f9dd1e52e5feb926a975fa7c1c5
VTU28087Nabeela <vtu28087@veltech.edu.in>
2026-09-10 10:29:27 +0530

    Delete WEEK1/RunningSumof1DArray.java

--- WEEK1/RunningSumof1DArray.java
+++
@@ -1,9 +0,0 @@
-public class RunningSumof1DArray {
-    public int[] runningSum(int[] arr) {
-        int n = arr.length;
-        for (int i =1; i<n;i++){
-            arr[i]=arr[i]+arr[i-1];
-        }
-        return arr;
-    }
-}
